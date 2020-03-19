b0b340153b163b6b7989eafd1d61c1839c409733
Maxwell Baird <bairdm71@gmail.com>
2020-03-19 09:00:21 -0600

    added a delete button

--- spec/features/shelters/user_can_delete_shelter_spec.rb
+++ spec/features/shelters/user_can_delete_shelter_spec.rb
@@ -2,19 +2,22 @@ require 'rails_helper'
 
 RSpec.describe "shelter index page", type: :feature do
   it "can delete a shelter" do
-    shelter_1 = Shelter.create(name: 'Broomfield Dog Shelter',
-                               address: '1111 fake dr.',
-                               city: 'Broomfield',
+    shelter_1 = Shelter.create(name: 'Laramie Frog Center',
+                               address: '1112 Main St.',
+                               city: 'Laramie',
+                               state: 'Wyoming',
+                               zip: '82072')
+    shelter_2 = Shelter.create(name: 'Boulder Frog Center',
+                               address: '1412 Main St.',
+                               city: 'Boulder',
                                state: 'Colorado',
-                               zip: '80020')
-     shelter_2 = Shelter.create(name: 'Laramie pet Shelter',
-                                address: '9999 false st.',
-                                city: 'Laramie',
-                                state: 'Wyoming',
-                                zip: '82072')
+                               zip: '82002')
     visit "/shelters"
     expect(page).to have_content(shelter_1.name)
     expect(page).to have_content(shelter_2.name)
-
+    visit "/shelters/#{shelter_1.id}"
+    click_button "delete"
+    expect(page).to have_content(shelter_2.name)
+    expect(current_path).to have_no_content(shelter_1.name)
   end
 end
