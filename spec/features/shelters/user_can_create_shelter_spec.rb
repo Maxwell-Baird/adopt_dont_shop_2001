55740a813f1c67dedb174e524e2242dcee35216e
Maxwell Baird <bairdm71@gmail.com>
2020-03-19 11:11:10 -0600

    updated create page with a new form

--- spec/features/shelters/user_can_create_shelter_spec.rb
+++ spec/features/shelters/user_can_create_shelter_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe "shelter new page", type: :feature do
     visit "/shelters"
     click_link 'New Shelter'
     expect(current_path).to eq('/shelters/new')
-    fill_in 'shelter[name]', with: 'New York Pet Shelter'
-    fill_in 'shelter[address]', with: '1111 fake st.'
-    fill_in 'shelter[city]', with: 'New York City'
-    fill_in 'shelter[state]', with: 'New York'
-    fill_in 'shelter[zip]', with: '10012'
+    fill_in 'name', with: 'New York Pet Shelter'
+    fill_in 'address', with: '1111 fake st.'
+    fill_in 'city', with: 'New York City'
+    fill_in 'state', with: 'New York'
+    fill_in 'zip', with: '10012'
     click_button ('submit')
 
     expect(current_path).to eq('/shelters')
