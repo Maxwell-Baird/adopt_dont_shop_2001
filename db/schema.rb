1cd96fee2c825271b057106707496d186d781253
Maxwell Baird <bairdm71@gmail.com>
2020-03-20 09:38:08 -0600

    created the pet index

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200318152318) do
+ActiveRecord::Schema.define(version: 20200320151727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pets", force: :cascade do |t|
+    t.string "name"
+    t.string "age"
+    t.string "sex"
+    t.string "shelter"
+    t.string "image"
+  end
 
   create_table "shelters", force: :cascade do |t|
     t.string "name"
--- spec/features/pets/user_can_see_all_the_pets_spec.rb
+++ spec/features/pets/user_can_see_all_the_pets_spec.rb
@@ -3,17 +3,29 @@ require 'rails_helper'
 RSpec.describe "pet index page", type: :feature do
   it "can see all pet names" do
     pet_1 = Pet.create(
-                        name: = 'Perry',
-                        age: = '12',
-                        sex: = 'Male',
-                        shelter: = 'Denver Hope Center',
-                        image: = 'https://images.app.goo.gl/pSMRBa7jXKCv6jnU6'
+                        name: 'Perry',
+                        age: '12',
+                        sex: 'Male',
+                        shelter: 'Denver Hope Center',
+                        image: 'http://pngimg.com/uploads/dog/dog_PNG50348.png'
+    )
+    pet_2 = Pet.create(
+                        name: 'Chanel',
+                        age: '7',
+                        sex: 'Female',
+                        shelter: 'Denver Hope Center',
+                        image: 'https://www.vhv.rs/file/max/36/361319_cute-dog-png.png'
     )
     visit "/pets"
-    expect(page).to have_content("Name: #{pet_1.name}")
+    expect(page).to have_content(pet_1.name)
     expect(page).to have_content("Age: #{pet_1.age}")
     expect(page).to have_content("Sex: #{pet_1.sex}")
     expect(page).to have_content("Shelter: #{pet_1.shelter}")
-    expect(page).to have_css("img[src*='https://images.app.goo.gl/pSMRBa7jXKCv6jnU6']")
+    expect(page).to have_css("img[src*='http://pngimg.com/uploads/dog/dog_PNG50348.png']")
+    expect(page).to have_content(pet_2.name)
+    expect(page).to have_content("Age: #{pet_2.age}")
+    expect(page).to have_content("Sex: #{pet_2.sex}")
+    expect(page).to have_content("Shelter: #{pet_2.shelter}")
+    expect(page).to have_css("img[src*='https://www.vhv.rs/file/max/36/361319_cute-dog-png.png']")
   end
-end'
+end
