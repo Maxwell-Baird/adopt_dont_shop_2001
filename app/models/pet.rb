1b586aa356e778dc6c4ec2906d666b8c92a75347
Maxwell Baird <bairdm71@gmail.com>
2020-03-22 15:41:39 -0600

    added default value for status

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,7 @@
 class Pet < ApplicationRecord
   belongs_to :shelter
-  
+  before_save :default_values
+  def default_values
+    self.status ||= 'adoptable' # note self.status = 'P' if self.status.nil? might be safer (per @frontendbeauty)
+  end
 end
