45702d06bdbc4bc62975e6f62bc00e9aea720545
Maxwell Baird <bairdm71@gmail.com>
2020-03-22 15:59:48 -0600

    created the test to check if you can delete from shelter index

---
+++ spec/features/shelters/user_can_delete_from_index_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "shelter index page", type: :feature do
+  it "can delete a shelter" do
+    shelter_1 = Shelter.create(name: 'Laramie Frog Center',
+                               address: '1112 Main St.',
+                               city: 'Laramie',
+                               state: 'Wyoming',
+                               zip: '82072')
+    shelter_2 = Shelter.create(name: 'Boulder Frog Center',
+                               address: '1412 Main St.',
+                               city: 'Boulder',
+                               state: 'Colorado',
+                               zip: '82002')
+    visit "/shelters"
+    expect(page).to have_content(shelter_1.name)
+    expect(page).to have_content(shelter_2.name)
+    click_button "Delete Shelter"
+    expect(page).to have_content(shelter_2.name)
+    expect(page).to have_no_content(shelter_1.name)
+  end
+end
