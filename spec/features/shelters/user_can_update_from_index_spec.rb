c2b52052cd21eb3c2f89c68054c6f5d9164a8377
Maxwell Baird <bairdm71@gmail.com>
2020-03-22 15:57:21 -0600

    add a test to check if you can update from shelter page

---
+++ spec/features/shelters/user_can_update_from_index_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "shelter index page", type: :feature do
+  it "can edit shelter details" do
+    shelter_1 = Shelter.create(name: 'Denver Dog Shelter',
+                               address: '8888 fake dr.',
+                               city: 'Denver',
+                               state: 'Colorado',
+                               zip: '80020')
+
+    visit "/shelters"
+    click_link 'Update Shelter'
+    expect(current_path).to eq("/shelters/#{shelter_1.id}/edit")
+  
+  end
+end
